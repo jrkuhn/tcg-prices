3824e68a8c9f6d431ae73c9fdc754e010bae1fd4
jrkuhn <jrkuhn@iastate.edu>
2018-12-22 07:49:59 -0600

    rough test 5

--- bot.js
+++ bot.js
@@ -56,7 +56,9 @@ async function handleCard(name) {
   var numPrices = prices.results.length;
   if(numPrices > 0) {
     for(i = 0; i < numPrices; i++) {
-      message += "\n[" + prices.results[i].subTypeName + "] low:$" + prices.results[i].lowPrice + "  market:$" + prices.results[i].marketPrice;
+      if(prices.results[i].subTypeName){ message += "\n[" + prices.results[i].subTypeName + "]"; }
+      if(prices.results[i].lowPrice) { message += " low:$" + prices.results[i].lowPrice; }
+      if(prices.results[i].marketPrice) { message += " market:$" + prices.results[i].marketPrice; }
     }
   }
   //need to getDetails of each id(in prices) for full name, card no., rarity
